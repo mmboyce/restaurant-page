041e2b403de5d1f3eabcd5769e50f5a973285278
mmboyce <mathieumimmsboyce@gmail.com>
2020-01-20 14:47:46 -0500

    Fill page with barebones look

---
+++ src/pageLoad.js
@@ -0,0 +1,101 @@
+import {carbLoadDesc, carbLoad, menuItems} from './restInfo'
+
+const content = document.querySelector("#content")
+
+
+function loadHeader() {
+    const header = document.createElement("div")
+    
+    header.id = "header"
+    
+    const title = document.createElement("h1")
+    title.textContent = carbLoad
+    
+    header.appendChild(title)
+
+    content.appendChild(header)
+    return header
+}
+
+function loadDescription() {
+    const description = document.createElement("div")
+    description.id = "description"
+
+    // add a new <p> element for each line of the description
+    for(let i = 0; i < carbLoadDesc.length; i++){
+        const descLine = document.createElement("p")
+        descLine.textContent = carbLoadDesc[i]
+
+        description.appendChild(descLine)
+    }
+
+    return description
+}
+
+function createMenuList(list, parentNode){
+    list.forEach(item => {
+        let name = item.name
+        let price = item.price
+
+        let node = document.createElement("li")
+        node.textContent = `${name}, \$${price}`
+
+        parentNode.appendChild(node)
+    })
+}
+
+function loadMenu() {
+    const menu = document.createElement("div")
+    menu.id = "menu"
+
+    const sandwiches = menuItems[0]
+    const pasta = menuItems[1]
+    const sides = menuItems[2]
+
+    const sandwichesTitle = document.createElement("h2")
+    sandwichesTitle.textContent = "Sandwiches"
+    const sandwichesList = document.createElement("ul")
+    createMenuList(sandwiches, sandwichesList)
+
+    const pastaTitle = document.createElement("h2")
+    pastaTitle.textContent = "Pasta"
+    const pastaList = document.createElement("ul")
+    createMenuList(pasta, pastaList)
+
+    const sidesTitle = document.createElement("h2")
+    sidesTitle.textContent = "Sides"
+    const sidesList = document.createElement("ul")
+    createMenuList(sides, sidesList)
+
+    menu.appendChild(sandwichesTitle)
+    menu.appendChild(sandwichesList)
+
+    menu.appendChild(pastaTitle)
+    menu.appendChild(pastaList)
+    
+    menu.appendChild(sidesTitle)
+    menu.appendChild(sidesList)
+
+    return menu
+}
+
+function loadBody() {
+    const body = document.createElement("div")
+
+    body.id = "body"
+
+    const description = loadDescription()
+    const menu = loadMenu()
+
+    content.appendChild(description)
+    content.appendChild(menu)
+    return body
+}
+
+function pageLoad() {
+    loadHeader()
+    loadBody()
+}
+
+
+export default pageLoad
---
+++ src/restInfo.js
@@ -0,0 +1,35 @@
+export const carbLoad = "Carb Load"
+
+const desc1 = `Welcome to ${carbLoad}, the best spot in town for pasta and bread!`
+const desc2 = `In this house, we pride ourselves on the finest baking and culinary talents around.`
+const desc3 = `Our staff has been rated as the best in hospitality FIVE YEARS IN A ROW by the Restaurant Board!`
+const desc4 = `So enjoy good eats and take a load off at the best spot in town, ${carbLoad}`
+
+const sandwiches = [
+    {name: 'Mac and Cheese Grilled Cheese', price: 12},
+    {name: 'Tator Tot Cheeseburger w/ Sour Cream', price: 10},
+    {name: 'Potato, Egg, & Chorizo Burrito', price: 9},
+    {name: 'Fried Pork Chop Sandwich', price: 9},
+    {name: 'Mango-Salsa & Turkey Bagel', price: 8},
+    {name: 'Peach, Prosciutto, & Avocado Sandwich', price: 7}
+]
+
+const pasta = [
+    {name: 'Chicken Carbonara', price: 13},
+    {name: 'Baked Ziti', price: 11},
+    {name: 'Parmesean Broccoli Fettucine', price: 11},
+    {name: 'Pumpkin-Chorizo Bow Ties', price: 9},
+    {name: 'Southern Barbecue Spaghetti', price: 8}
+]
+
+const sides = [
+    {name: 'Pickled Beets', price: 4},
+    {name: 'Blue Cheese Sweet Potato Fries', price: 4},
+    {name: 'Garlic-Chive Baked Fries', price: 3},
+    {name: 'Garlic Mashed Potatoes', price: 2}
+]
+
+export const carbLoadDesc = [desc1, desc2, desc3, desc4]
+export const menuItems = [
+    sandwiches, pasta, sides
+]
